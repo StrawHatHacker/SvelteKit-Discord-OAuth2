4caecb745e5f2aabdc87817ae53f40edad5186cb
strawhathacker <ntahsp@gmail.com>
2022-07-19 15:29:58 +0300

    Logout flow

---
+++ src/routes/api/v1/user/logout.ts
@@ -0,0 +1,21 @@
+import { deleteSession } from '$lib/utils/sessionHandler';
+import cookie from 'cookie';
+
+export async function post({ request }) {
+    const cookies = cookie.parse(request.headers.get('cookie') || '');
+
+    if (cookies['session_id']) deleteSession(cookies['session_id']);
+
+    return {
+        status: 200,
+        headers: {
+            'Set-Cookie': cookie.serialize('session_id', null, {
+                path: '/',
+                httpOnly: true,
+                sameSite: false,
+                secure: true,
+                maxAge: 0
+            })
+        }
+    };
+}
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import { UserStore } from '$lib/stores';
+
+	async function logout() {
+		await fetch('/api/v1/user/logout', {
+			method: 'POST',
+			body: JSON.stringify({}),
+		});
+
+		UserStore.setUser(null);
+
+		window.location.href = '/';
+	}
 </script>
 
 <p>User's Discord ID: {$UserStore.id}</p>
+
+<button on:click={logout}>Logout</button>
 
